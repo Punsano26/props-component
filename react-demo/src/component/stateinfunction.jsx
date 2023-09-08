9b34dd62a2e1994a40eb287e5f5a35d5c127b875
Punsan026 <xlichaphlreuxn@gmail.com>
2023-09-08 14:29:53 +0700

    tride

--- react-demo/src/component/stateinfunction.jsx
+++ react-demo/src/component/stateinfunction.jsx
@@ -1,32 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-const StateInFunction = () => {
-    const SateInFunction = (props) => {
-        const [data, setData] = useState({
-            id:props.id,
-            name:props.name,
-            counter:0
-        })
-        const pluseFunction = () => {
-            setData({...data,counter:data.counter+1});
-        };
-        const minusFunction = () => {
-            setData({...data,counter:data.counter - 1});
-        };
-    }
-  return (
-     <div>
-                <h1>State In class Component</h1>
-                <p>
-                    <b>Name:</b> {this.state.name}
-                </p>
-                <p>
-                    <b>Counter:</b> {this.state.counter}
-                </p>
-                <button onClick={this.pluseFunction}>+</button>
-                <button onClick={this.minusFunction}>-</button>
-            </div>
-  )
-}
+const StateInFunction = (props) => {
+    const [data, setData] = useState({
+        id: props.id,
+        name: props.name,
+        counter: 0
+    });
 
-export default StateInFunction
+    const pluseFunction = () => {
+        setData({ ...data, counter: data.counter + 1 });
+    };
+
+    const minusFunction = () => {
+        setData({ ...data, counter: data.counter - 1 });
+    };
+
+    return (
+        <div>
+            <h1>State In Functional Component</h1>
+            <p>
+                <b>Name:</b> {data.name}
+            </p>
+            <p>
+                <b>Counter:</b> {data.counter}
+            </p>
+            <button onClick={pluseFunction}>+</button>
+            <button onClick={minusFunction}>-</button>
+        </div>
+    );
+};
+
+export default StateInFunction;
